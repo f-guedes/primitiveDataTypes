b9fb83ee7934d3e85fad6b1464de5804b3384c78
f-guedes <fernando.hpg2@gmail.com>
2022-09-02 00:12:22 -0400

    Added project with some primitive data types

---
+++ src/primitiveDataTypes/Application.java
@@ -0,0 +1,42 @@
+package primitiveDataTypes;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this is a comment, and will be ignored
+				/*
+				 * primitive - just a piece of data (8 types):
+				 * int - an integer (whole number)
+				 * double - can have decimals
+				 * float - precision decimal data type. Like a double, but more precise.
+				 * long - like an int, but with higher ranges (higher numbers)
+				 * short - opposite of long. Like an ing, but with less of number range
+				 * byte - 8 bits of data
+				 * char - a single character (a, B, A, &)
+				 * boolean - can only be 2 values, true or false 
+				 * 
+				 * objects
+				 * string - textual data, a string of characters; characters strung together
+				 * 	
+				 */
+				
+				//variable declaration - type, identifier, assignment operator, value, semicolon
+				int samsAge = 32;
+				int numberofFriends = 310; 
+				double Balance = 34.67;
+				char middleInitial = 'C';
+				boolean isHotOutside = false;
+				String firstName = "Sam";
+						
+						
+						
+				System.out.println(samsAge);
+				System.out.println(numberofFriends);
+				System.out.println(Balance);
+				System.out.println(middleInitial);
+				System.out.println(isHotOutside);
+				System.out.println(firstName);
+	}
+
+}
